3c6481fea8cdc0eb49f65445ba2f9bf7e5eb73dd
samueeeel9876 <andrade_samuel607@escola.pr.gov.br>
2026-05-05 09:29:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Seleciona o botão pelo ID
+const botaoApetite = document.getElementById('btn-apetite');
+
+// Adiciona um evento de clique
+botaoApetite.addEventListener('click', function() {
+    alert("Bom apetite! Prepare o café!");
+});
